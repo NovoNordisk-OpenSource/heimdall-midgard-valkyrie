9e462e1ecb3719d8c50c6a7e889b7a3cf11a4423
Tobias Andersen <tobias.andersen79@gmail.com>
2024-04-08 12:15:24 +0200

    fixed mocking issue in update test

--- tests/Heimdall.Midgard.Valkyrie.Domain.UnitTest/Services/ScaffoldServiceTests.cs
+++ tests/Heimdall.Midgard.Valkyrie.Domain.UnitTest/Services/ScaffoldServiceTests.cs
@@ -14,7 +14,6 @@ public class ScaffoldServiceTests
 
         //Assert
         Assert.NotNull(sut);
-
         Mock.VerifyAll();
     }
 
@@ -37,7 +36,6 @@ public class ScaffoldServiceTests
 
         //Assert
         Assert.NotNull(result);
-
         Mock.VerifyAll();
     }
 
@@ -57,11 +55,60 @@ public class ScaffoldServiceTests
         var sut = new ScaffoldService(mockRepository.Object);
 
         //Act
-        await sut.DeleteScaffoldTaskAsync(Guid.NewGuid());
+        var result = await sut.DeleteScaffoldTaskAsync(Guid.NewGuid());
 
         //Assert
+        Assert.False(result);
         Mock.VerifyAll();
     }
+
+    [Fact]
+    public async Task CanUpdateScaffoldTask()
+    {
+        //Arrange
+        var mockUnitOfWork = new Mock<IUnitOfWork>();
+        var mockRepository = new Mock<IScaffoldTaskRepository>();
+        var fakeAggregate = new ScaffoldTask(new AccountInfo("default", "default"));
+
+        mockUnitOfWork.Setup(m => m.SaveChangesAsync(It.IsAny<CancellationToken>())).Returns(Task.FromResult(1));
+        mockRepository.SetupGet(m => m.UnitOfWork).Returns(mockUnitOfWork.Object);
+        mockRepository.Setup(m => m.GetAsync(It.IsAny<Guid>(), It.IsAny<CancellationToken>())).Returns(Task.FromResult(fakeAggregate));
+        mockRepository.Setup(m => m.Update(It.IsAny<ScaffoldTask>())).Returns(fakeAggregate);
+
+        var sut = new ScaffoldService(mockRepository.Object);
+
+        //Act
+        var result = await sut.UpdateScaffoldTaskAsync(fakeAggregate);
+
+        //Assert
+        Assert.NotNull(result);
+        Mock.VerifyAll();
+    }
+
+    [Fact]
+    public async Task CanAddOrUpdateScaffoldOption()
+    {
+        //Arrange
+        var mockUnitOfWork = new Mock<IUnitOfWork>();
+        var mockRepository = new Mock<IScaffoldTaskRepository>();
+        var fakeAggregate = new ScaffoldTask(new AccountInfo("default", "default"));
+
+        mockUnitOfWork.Setup(m => m.SaveChangesAsync(It.IsAny<CancellationToken>())).Returns(Task.FromResult(1));
+        mockRepository.SetupGet(m => m.UnitOfWork).Returns(mockUnitOfWork.Object);
+        mockRepository.Setup(m => m.GetAsync(It.IsAny<Guid>(), It.IsAny<CancellationToken>())).Returns(Task.FromResult(fakeAggregate));
+        mockRepository.Setup(m => m.Update(It.IsAny<ScaffoldTask>()));
+
+        var sut = new ScaffoldService(mockRepository.Object);
+
+        //Act
+        var result = await sut.AddOrUpdateScaffoldOptionAsync(fakeAggregate.Id, "key", "value");
+
+        //Assert
+        Assert.NotNull(result);
+        Assert.True(result.Key == "key" && result.Value == "value");
+        Mock.VerifyAll();
+    }
+
 
     [Fact]
     public async Task CanDeleteScaffoldOption()
@@ -79,9 +126,10 @@ public class ScaffoldServiceTests
         var sut = new ScaffoldService(mockRepository.Object);
 
         //Act
-        await sut.DeleteScaffoldOptionAsync(fakeAggregate.Id, "option-key");
+        var result = await sut.DeleteScaffoldOptionAsync(fakeAggregate.Id, "key");
 
         //Assert
+        Assert.True(result);
         Mock.VerifyAll();
     }
 }
---
+++ tests/Heimdall.Midgard.Valkyrie.Domain.UnitTest/ValueObjects/AccountInfoTests.cs
@@ -0,0 +1,47 @@
+﻿namespace Heimdall.Midgard.Valkyrie.Domain.ValueObjects;
+
+public class AccountInfoTests
+{
+    [Fact]
+    public void CanBeConstructed()
+    {
+        //Arrange
+        AccountInfo sut;
+
+        //Act
+        sut = new AccountInfo("identifier", "role");
+
+        //Assert
+        Assert.NotNull(sut);
+        Assert.Equal("identifier", sut.Identifier);
+        Assert.Equal("role", sut.Role);
+    }
+
+    [Fact]
+    public void CanBeSerialized()
+    {
+        //Arrange
+        var sut = new AccountInfo("identifier", "role");
+
+        //Act
+        var payload = JsonSerializer.Serialize(sut, new JsonSerializerOptions { DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull });
+
+        //Assert
+        Assert.NotNull(JsonDocument.Parse(payload));
+    }
+
+    [Fact]
+    public void CanBeDeserialized()
+    {
+        //Arrange
+        AccountInfo sut;
+
+        //Act
+        sut = JsonSerializer.Deserialize<AccountInfo>("{\"identifier\":\"identifier\",\"role\":\"role\"}");
+
+        //Assert
+        Assert.NotNull(sut);
+        Assert.Equal("identifier", sut.Identifier);
+        Assert.Equal("role", sut.Role);
+    }
+}
